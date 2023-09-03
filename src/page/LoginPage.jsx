bb83d2a4f683f87eb92c195a47aff9eaa9e4eab6
VirLg <19virlg@gmail.com>
2023-09-03 19:30:33 +0300

    add

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [name, setName] = useState('');
+  const [email, setemail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = e => {
+    console.log('e.target.name', e.target.name);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input onChange={handleChange} value={name} type="text" name="name" />
+      </label>
+      <label>
+        Email
+        <input onChange={handleChange} value={email} type="text" name="email" />
+      </label>
+      <label>
+        Password
+        <input
+          onChange={handleChange}
+          value={password}
+          type="text"
+          name="password"
+        />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
 };
 
 export default LoginPage;
