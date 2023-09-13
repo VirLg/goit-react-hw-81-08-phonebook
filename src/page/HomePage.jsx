ce5597759090ebb0fb65f0ebd6b61acf3bbbebaa
VirLg <19virlg@gmail.com>
2023-09-13 15:42:34 +0300

    add

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import contact from '../contact.png';
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="container-form">
+      <img
+        src={contact}
+        alt=""
+        style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
